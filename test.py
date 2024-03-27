0ac1dc7829d8601a23c4268e6c573086dd854c88
sher731053 <125851448+sher731053@users.noreply.github.com>
2024-03-27 10:11:58 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+print("hello ")
+
+name="Sher Singh"
+age= 19
+salary=500000
+employee_status='p'
+
+
+#print the value of a variable
+print("Name : "+name+"\t Age : ",age)
+print("Name :", name)
+
+print("Employee name: ", name,end="  ")
+print("-----------------------------")
+
+print("Employee name: ", name,end="\n\n\n")
+print("-----------------------------")
